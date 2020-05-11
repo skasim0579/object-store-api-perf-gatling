fdb4f27e3e2671584fd270992b274da2413ed176
abdulkadir <skasim0579@gmail.com>
2020-05-11 15:01:38 -0400

    Updated simulation file

--- src/test/scala/ca/gc/aafc/objectstore/api/test/perf/gatling/FileUploadDownloadSimulation.scala
+++ src/test/scala/ca/gc/aafc/objectstore/api/test/perf/gatling/FileUploadDownloadSimulation.scala
@@ -13,12 +13,12 @@ import java.io.BufferedReader
 import scala.io.Source.fromURL
 import java.io.File
 import java.io._
+import java.nio.charset.StandardCharsets
 
 class FileUploadDownloadSimulation extends Simulation {
   var fileId: String = null;
   var bucket: String = "mybucket2";
   var jsonFileInStr: String = "";
-  val url = getClass.getResource("/metadata1.json")
 
   private def getProperty(propertyName: String, defaultValue: String) = {
     Option(System.getenv(propertyName))
@@ -29,39 +29,21 @@ class FileUploadDownloadSimulation extends Simulation {
   def saveFileId(identifier: String): Unit = {
     fileId = identifier;
     println("fileIdentifier: " + identifier)
-    println("fileId: " + fileId)
-    if (url != null) {
-      val source = Source.fromURL(url)
-      jsonFileInStr = source.getLines().mkString
-      jsonFileInStr = jsonFileInStr.replaceFirst("bucket1", bucket)
-      jsonFileInStr = jsonFileInStr.replaceFirst("fileIdentifier1", fileId)
-      println("jsonFileInStr: " + jsonFileInStr)
-      val pw = new PrintWriter(new File("metadata2.json"))
-      pw.write(jsonFileInStr)
-      pw.close
-    }
   }
 
-  def writeToFile(response: String): Unit = {
-    var writer = null
-    try {
-      var writer: BufferedWriter = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("/home/abdulkadir/gat-ws/mypngfile1.png")))
-      writer.write(response)
-      writer.close()
-    } catch {
-      case e: IOException =>
-    }
-  }
+  val customFeeder = Iterator.continually(Map(
+    "bucket1" -> bucket,
+    "fileId1" -> fileId,
+  ))
+  
   def serverHost: String = getProperty("server.host", "localhost")
   def serverPort: String = getProperty("server.port", "8081")
   println("serverHost: " + serverHost)
   println("serverPort: " + serverPort)
-//    var fileIdentifier: String = "8809ed21-d5ef-4cb2-b51a-9a60ae09a89e"
-//    saveFileId(fileIdentifier)
 
   val httpProtocol = http
     .baseUrl("http://" + serverHost + ":" + serverPort) // Here is the root for all relative URLs
-
+  //
   val scn =
     scenario("post-fileUploadDownload") // A scenario is a chain of requests and pauses
       // -----------------------------
@@ -73,20 +55,15 @@ class FileUploadDownloadSimulation extends Simulation {
         .check(jsonPath("$.fileIdentifier").saveAs("fileIdentifier")))
       .exec { session => saveFileId(session("fileIdentifier").as[String]); session }
       // -----------------------------
+      .feed(customFeeder)
       .exec(http("Post-metadata")
         .post("/api/v1/metadata")
         .header(HttpHeaderNames.ContentType, "application/vnd.api+json")
         .header(HttpHeaderNames.Accept, "application/vnd.api+json")
-        .body(RawFileBody("./metadata2.json"))
+        .body(ElFileBody("metadata1.json"))
         .check(status.is(201))
         .check(bodyString.saveAs("responseData2")))
       .exec { session => println("metadata-responseData: " + session("responseData2").as[String]); session }
-  // -----------------------------
-  //        .exec(http("Get-fileDownload")
-  //          .get("/api/v1/file/" + bucket + "/" + fileId)
-  //          .check(status.is(200))
-  //          .check(bodyString.saveAs("responseBody")))
-  //        .exec { session => writeToFile(session("responseBody").as[String]); session }
 
   setUp(scn.inject(atOnceUsers(1)).protocols(httpProtocol))
 }
